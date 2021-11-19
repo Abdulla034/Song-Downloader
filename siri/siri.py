e37da0ae9753a42acb1f180a99d8dbf68b4eb7d9
Abdulla034 <71590852+Abdulla034@users.noreply.github.com>
2021-11-19 13:23:39 +0400

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -16,7 +16,7 @@ def time_to_seconds(time):
 
 @Client.on_message(filters.command('start') & filters.private)
 async def start(client, message):
-    await message.reply(CONFIG.START_MSG.format(message.from_user.mention),
+    await message.reply(Config.START_MSG.format(message.from_user.mention),
          reply_markup=InlineKeyboardMarkup(
              [
                 [
