73880cea4a9129476dbf61fd5aa3f05d11a617c6
Abdulla034 <71590852+Abdulla034@users.noreply.github.com>
2021-11-19 12:02:55 +0400

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -16,7 +16,7 @@ def time_to_seconds(time):
 
 @Client.on_message(filters.command('start') & filters.private)
 async def start(client, message):
-    await message.reply_photo(photo=Config.START_IMG, caption=Config.START_MSG.format(message.from_user.mention),
+    await message.reply_text(caption=Config.START_MSG.format(message.from_user.mention),
          reply_markup=InlineKeyboardMarkup(
              [
                 [
