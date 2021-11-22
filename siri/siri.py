b773b334893112072b520eec5eaee90dde60734f
Abdulla034 <71590852+Abdulla034@users.noreply.github.com>
2021-11-22 05:18:31 +0400

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -88,7 +88,7 @@ def a(client, message):
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
             secmul *= 60
-        await shed.edit("📤 Uploading...")
+        m.edit("`📤 Uploading...`")
         message.reply_audio(
             audio_file,
             caption=rep,
