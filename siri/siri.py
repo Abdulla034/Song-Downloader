4742faa8928ddb7f0d121057c99d51aeb19d26dc
Abdulla034 <71590852+Abdulla034@users.noreply.github.com>
2021-11-26 01:35:39 +0400

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -80,9 +80,9 @@ def a(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
         rep = f"""
- 🎵 Başlıq {title}
- ⏱️ Müddət {duration}
- 👁 Baxış sayı {results[0]['views']}
+  🎵 Başlıq {title}
+  ⏱️ Müddət {duration}
+  👁 Baxış sayı {views}
 """
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
