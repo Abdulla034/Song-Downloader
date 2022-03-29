2c4358dcf93dff8686af5eb56727ef9bfdb76401
Abdulla034 <71590852+Abdulla034@users.noreply.github.com>
2022-03-29 18:54:55 +0400

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,5 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name with /song command")
     OWNER = os.environ.get("OWNER", "shamilhabeeb")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-    GROUP_SUPPORT = os.environ.get("GROUP_SUPPORT", "Repo")
-    GONDERME_TURU = os.environ.get("GONDERME_TURU", False)
-    OWNER_ID = int(os.environ.get("OWNER_ID"))
-    LANGAUGE = os.environ.get("LANGAUGE", "AZ")
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}
