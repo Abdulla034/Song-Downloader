3a67fc54f222cedb66ba2f64e12a71e8475471cf
Abdulla034 <71590852+Abdulla034@users.noreply.github.com>
2022-03-29 19:17:41 +0400

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,6 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name with /song command")
     OWNER = os.environ.get("OWNER", "shamilhabeeb")
+    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID"))
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}
